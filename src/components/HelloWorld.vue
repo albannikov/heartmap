f7f327053bdeab9d56b7a3a12f443648b785d8c1
albannikov <albannikov@yandex.ru>
2022-07-14 19:12:45 +0500

    fetch data from link

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,35 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue';
+import Button from './Button.vue'
 
 defineProps({
   msg: String
+});
+
+const count = ref(0);
+const name = ref("");
+const posts = ref([]);
+const loading = ref(false);
+
+async function getData() {
+  loading.value = true;
+  const repsponse = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const data = await repsponse.json();
+  posts.value = data;
+
+  setTimeout(() => {
+    loading.value = false;
+  }, 1500);
+}
+
+function onClick() {
+  console.log("Click");
+}
+
+onMounted(async () => {
+  await getData();
 })
 
-const count = ref(0)
 
 
  
@@ -37,7 +61,55 @@ const count = ref(0)
     <code>components/HelloWorld.vue</code> to test hot module replacement.
   </p>
 
-  
+  <h1>{{ msg }}</h1>
+
+  Name is: {{ name }}
+
+  <div>
+    <input type="text" v-model="name">
+  </div>
+
+  <Button 
+    @click="getData" 
+    :variant="'success'" 
+    :disabled="loading"
+  >
+    Get data
+  </Button>
+
+  <Button @click="onClick">
+    Upload
+  </Button>
+
+  <Button>
+    Delete
+  </Button>
+
+  <div v-if="loading"> 
+    <h3>Загузка</h3>
+  </div>
+  <div v-else>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <th>User id</th>
+          <th>id</th>
+          <th>Title</th>
+          <th>Body</th>
+        </tr>
+      </thead>
+      <tbody>
+
+        <tr v-for="post in posts">
+          <td>{{ post.userId }}</td>
+          <td>{{ post.id }}</td>
+          <td>{{ post.title }}</td>
+          <td>{{ post.body }}</td>
+        </tr>
+
+      </tbody>
+    </table>
+  </div>
 </template>
 
 <style scoped>
