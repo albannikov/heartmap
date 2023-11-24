cb1f6799235153f33567a6d8ac4e5fee77c55e1b
albannikov <104481138+albannikov@users.noreply.github.com>
2023-11-24 08:50:11 +0500

    redesign

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -5,10 +5,7 @@ import router from '../router.js';
 const snackbar = useSnackbar();
 
 
-/*
-* Функция аутентификации
-* Получим статус переменной с бэкенда и обработаем
-*/
+/* Функция аутентификации. Получим статус переменной с бэкенда и обработаем */
 
 async function Auth(){
   let login = document.getElementById("userName").value;
@@ -50,10 +47,12 @@ let result = await response.json();
 </script>
 
 <template>
-    <body class="authBody">
-      <div class="authBodyBack">    
+      <div id="stars-container">
+        <div id='stars'></div>
+			<div id='stars2'></div>
+			<div id='stars3'></div>
    
-      </div>
+
       <div class="loginForm">
         <div class="logo"></div>	
         <label>Логин</label>
@@ -63,14 +62,214 @@ let result = await response.json();
           <div class="div_center">
             <Button class="loginButton"
               @click="Auth" 
-              :variant="'btn btn-primary btn-lg'"
+              :variant="'btn btn-dark btn-lg'"
               >Войти в систему
             </Button>
           </div>
-          <div class="copyright">
-            Аналитическая информационная система "Умные карты"<br>
-            г. Радужный, ХМАО-Югра
-          </div>
-      </div>
-    </body>
+          <div class="copyright">АИС «Умные карты»</div>
+        </div>
+
+	
+</div>
 </template>
+
+<style lang="scss" scoped>
+
+.logo {
+  margin: auto;  /* Magic! */
+	width: 240px;
+	height: 100px;
+	background-image: url(../img/bear.png);
+  background-position: top center;
+	background-size: 140px;
+	background-repeat: no-repeat;
+
+}
+
+
+.loginForm {  
+  text-align:center;
+	border: 1px solid;
+  border-radius: 5px;
+	border-color: rgb(136, 136, 136);
+	width: fit-content;
+	padding: 30px 45px 10px 45px;
+	/* border-radius: 10px; */
+	background-color: rgba(255, 255, 255, 0.9);
+	// backdrop-filter: blur(150px);
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+	box-shadow: 0 0 150px rgba(255, 255, 255, 0.2);
+}
+
+.loginInput {
+	height: 38px;
+	/* border-radius: 5px; */
+	border: 1px solid;
+	border-color: rgb(136, 136, 136);
+	width: 100%;	
+	font-size: 18px;
+	margin-bottom: 20px;
+}
+
+.loginButton {
+	margin-bottom: 30px;
+}
+
+@mixin translate50
+{
+  -webkit-transform: translate(-50, -50%);
+  -ms-transform: translate(-50, -50%);
+  -o-transform: translate(-50, -50%);
+  transform: translate(-50, -50%);
+}
+
+@mixin roundedCorners
+{
+  -webkit-border-radius: 50%; 
+  -moz-border-radius: 50%; 
+  border-radius: 50%;
+}
+
+@mixin rotateBase
+{
+  -webkit-transform: rotate3d(-1, 1, 0, 0deg);
+  -ms-transform: rotate3d(-1, 1, 0, 0deg);
+  -o-transform: rotate3d(-1, 1, 0, 0deg);
+  transform: rotate3d(-1, 1, 0, 0deg);
+}
+
+@mixin rotateRight
+{
+  -webkit-transform: rotate3d(-1, 1, 0, 30deg);
+  -ms-transform: rotate3d(-1, 1, 0, 30deg);
+  -o-transform: rotate3d(-1, 1, 0, 30deg);
+  transform: rotate3d(-1, 1, 0, 30deg);
+}
+
+@mixin rotateLeft
+{
+  -webkit-transform: rotate3d(-1, 1, 0, -30deg);
+  -ms-transform: rotate3d(-1, 1, 0, -30deg);
+  -o-transform: rotate3d(-1, 1, 0, -30deg);
+  transform: rotate3d(-1, 1, 0, -30deg);
+}
+
+// n is number of stars generated
+@function generateStars ($n)
+{ 
+  $value: '#{0} #{random(2000)}px #{random(2000)}px #FFF';
+
+  @for $i from 2 through $n
+  {
+    $value: '#{$value} , #{random(2000)}px #{random(2000)}px #FFF';
+  }
+
+  @return unquote($value);
+}
+
+$stars-small:  generateStars(700);
+$stars-medium: generateStars(200);
+$stars-big:    generateStars(100);
+
+body
+{
+  overflow: hidden;
+}
+
+#stars-container
+{
+  height: 100vh;
+  width: 100%;
+  background: radial-gradient(ellipse at bottom, #1B2735 0%, #191F24 100%);
+  overflow: hidden;
+  @include translate50;
+}
+    
+#stars
+{
+  width: 1px;
+  height: 1px;
+  background: transparent;
+  box-shadow: $stars-small;
+  animation : animateStars 28.5s ease-in-out infinite;
+  @include roundedCorners;
+
+  &::after
+  {
+    content: " ";
+    position: absolute;
+    margin: auto;
+    top: 0; left: 0; right: 0; bottom: 0;
+    width: 1px;
+    height: 1px;
+    background: transparent;
+    box-shadow: $stars-small;
+    @include roundedCorners;
+  }
+}
+    
+#stars2
+{
+  width: 2px;
+  height: 2px;
+  background: transparent;
+  box-shadow: $stars-medium;
+  animation : animateStars 30s ease-in-out infinite;
+  @include roundedCorners;
+    
+  &::after
+  {
+    content: " ";
+    position: absolute;
+    margin: auto;
+    top: 0; left: 0; right: 0; bottom: 0;
+    width: 2px;
+    height: 2px;
+    background: transparent;
+    box-shadow: $stars-medium;
+    @include roundedCorners;
+  }
+}
+    
+#stars3
+{
+  width: 3px;
+  height: 3px;
+  background: transparent;
+  box-shadow: $stars-big;
+  animation : animateStars 31.5s ease-in-out infinite;
+  @include roundedCorners;
+    
+  &:after
+  {
+    content: " ";
+    position: absolute;
+    margin: auto;
+    top: 0; left: 0; right: 0; bottom: 0;
+    width: 3px;
+    height: 3px;
+    background: transparent;
+    box-shadow: $stars-big;
+    @include roundedCorners;
+  }
+}
+
+@keyframes animateStars
+{
+  0%{@include rotateBase;}
+  25%{@include rotateRight;}
+  50%{@include rotateBase;}
+  75%{@include rotateLeft;}
+  100%{@include rotateBase;}
+}
+
+.copyright{
+  color: #383838;
+  position: relative;
+  display: block;
+  font-size: 13px;
+}
+</style>
--- src/components/navbar_element.vue
+++ src/components/navbar_element.vue
@@ -1,37 +1,22 @@
+
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+<nav class="navbar navbar-expand-lg navbar-light mt-2">
+  <div class="container-sm">
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">111Главная</a>
+    <div class="collapse navbar-collapse m-0 p-0" id="navbarSupportedContent">
+      <ul class="navbar-nav nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item w-100">          
+          <a class="nav-link active d-flex p-3 btn-light " href="#"><i class="fa-solid fa-map-location-dot m-1"></i> Тепловая карта</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
+        <li class="nav-item w-100">          
+          <a class="nav-link d-flex p-3" href="#"><i class="fa-solid fa-car-burst m-1"></i> Проишествия</a>
         </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
+        <li class="nav-item w-100">          
+          <a class="nav-link d-flex p-3" href="#"><i class="fa-solid fa-magnifying-glass-chart m-1"></i> Аналитика</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-2" type="Поиск" placeholder="Поиск" aria-label="Поиск">
-        <button class="btn btn-outline-success" type="submit">Поиск</button>
-      </form>
+       </ul>
     </div>
   </div>
 </nav>
